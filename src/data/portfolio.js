ae25ea5911af2ef2738d61e7b7c84fa3c176b0c0
widyafebriandaru <76604668+widyafebriandaru@users.noreply.github.com>
2023-05-11 10:01:04 +0700

    fix button color bg and text

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,20 @@
 export default [
-    {
-       title: 'Harijumat.com',
-       imgUrl: '/assets/1.png',
-       stack: ['HTML', 'CSS', 'JavaScript','Tailwind'],
-       link: 'https://widyafebriandaru.github.io/Tugas-Rise-HTML-Tailwind/',
-    },
-    {
-       title: 'Batu Gunting Kertas',
-       imgUrl: '/assets/2.png',
-       stack: ['HTML', 'CSS', 'Javascript'],
-       link: 'https://widyafebriandaru.github.io/Gunting-Batu-Kertas/',
-    },
-    {
-       title: 'Image Slider',
-       imgUrl: '/assets/3.png',
-       stack: ['HTML', 'CSS', 'Javascript'],
-       link: 'https://widyafebriandaru.github.io/Image-Slider/',
-    },
- ]
+  {
+    title: "Harijumat.com",
+    imgUrl: "/assets/1.png",
+    stack: ["HTML", "CSS", "JavaScript", "Tailwind"],
+    link: "https://widyafebriandaru.github.io/Tugas-Rise-HTML-Tailwind/",
+  },
+  {
+    title: "Batu Gunting Kertas",
+    imgUrl: "/assets/2.png",
+    stack: ["HTML", "CSS", "Javascript"],
+    link: "https://widyafebriandaru.github.io/Gunting-Batu-Kertas/",
+  },
+  {
+    title: "Image Slider",
+    imgUrl: "/assets/3.png",
+    stack: ["HTML", "CSS", "Javascript"],
+    link: "https://widyafebriandaru.github.io/Image-Slider/",
+  },
+];
